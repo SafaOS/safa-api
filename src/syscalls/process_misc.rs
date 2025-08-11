bebe44accee159218cd134a284964895a588f8f3
safiworks <116169309+safiworks@users.noreply.github.com>
2025-08-11 06:52:26 +0300

    chore(syscalls::prcoess_misc): remove sbrk's implementation

--- src/syscalls/process_misc.rs
+++ src/syscalls/process_misc.rs
@@ -6,10 +6,6 @@ use super::{define_syscall, err_from_u16, SyscallNum};
 extern crate alloc;
 
 define_syscall! {
-    SyscallNum::SysPSbrk => {
-        /// Increases the range of the process's data break by `size` bytes and puts the new break pointer in `target_ptr`
-        syssbrk(size: isize, target_ptr: OptZero<FFINonNull<*mut u8>>)
-    },
     SyscallNum::SysPCHDir => {
         /// Changes the current working directory to the path `buf` with length `buf_len`
         /// (expects given buffer to be utf-8)
@@ -25,17 +21,6 @@ define_syscall! {
 }
 
 #[inline]
-/// Increases the range of the process's data break by `size` bytes
-/// returns the new break pointer
-///
-/// you should probably use [`crate::alloc::GLOBAL_SYSTEM_ALLOCATOR`] instead for allocating memory
-pub fn sbrk(size: isize) -> Result<*mut u8, ErrorStatus> {
-    let mut target_ptr: *mut u8 = core::ptr::null_mut();
-    let ptr = RequiredPtrMut::new(&raw mut target_ptr).into();
-    err_from_u16!(syssbrk(size, ptr), target_ptr)
-}
-
-#[inline]
 /// Changes the current work dir to `path`
 pub fn chdir(path: &str) -> Result<(), ErrorStatus> {
     err_from_u16!(syschdir(Str::from_str(path)))
@@ -44,8 +29,6 @@ pub fn chdir(path: &str) -> Result<(), ErrorStatus> {
 use alloc::string::String;
 use alloc::vec::Vec;
 use safa_abi::errors::ErrorStatus;
-use safa_abi::ffi::option::OptZero;
-use safa_abi::ffi::ptr::FFINonNull;
 use safa_abi::ffi::slice::Slice;
 use safa_abi::ffi::str::Str;
 
